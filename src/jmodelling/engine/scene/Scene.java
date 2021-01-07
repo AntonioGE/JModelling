4236ef3f9a14a3d8bccb50f87d5fd215a48d29f0
AntonioGE <antonio.garcia.estraviz@gmail.com>
2021-01-07 20:39:26 +0100

    Added renderer and scene

---
+++ src/jmodelling/engine/scene/Scene.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmodelling.engine.scene;
+
+import java.util.HashMap;
+import jmodelling.engine.object.Object3D;
+import jmodelling.engine.object.camera.CamArcball;
+
+/**
+ *
+ * @author ANTONIO
+ */
+public class Scene {
+    
+    public HashMap<String, Object3D> objects;
+    
+    public Scene(){
+        
+    }
+    
+}

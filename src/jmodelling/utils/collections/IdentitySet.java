38e7eeccbecc62a0110459909b55c5c0f4e77e78
AntonioGE <antonio.garcia.estraviz@gmail.com>
2021-02-17 15:11:20 +0100

    Added Identity Set

---
+++ src/jmodelling/utils/collections/IdentitySet.java
@@ -0,0 +1,134 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 Antonio GE
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package jmodelling.utils.collections;
+
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author ANTONIO
+ */
+public class IdentitySet<E> extends AbstractSet<E> implements Set<E> {
+
+    private Map<E, Object> map;
+    private static final Object PRESENT = new Object();
+
+    public IdentitySet(int size) {
+        this.map = new IdentityHashMap<>(size);
+    }
+
+    public IdentitySet(){
+        this(16);
+    }
+    
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.containsKey(o);
+    }
+
+    @Override
+    public Iterator iterator() {
+        return map.keySet().iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return map.keySet().toArray();
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return map.keySet().toArray(a);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return c.stream().noneMatch((o) -> (!contains(o)));
+    }
+
+    /*
+    @Override
+    public boolean addAll(Collection<E> c) {
+        boolean changed = false;
+        for (E o : c) {
+            changed = changed || add(o);
+        }
+        return changed;
+
+    //Not tested
+    @Override
+    public boolean retainAll(Collection c) {
+        boolean changed = false;
+        Iterator ite = iterator();
+        while (ite.hasNext()) {
+            if (!c.contains(ite.next())) {
+                ite.remove();
+                changed = true;
+            }
+        }
+        return changed;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        boolean remove = false;
+        for (Object o : c) {
+            remove = remove || remove(o);
+        }
+        return remove;
+    }
+    */
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+}
